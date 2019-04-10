359266eefca3e12e9f0f753550bea279a59a9b6a
Christian Dallago <code@dallago.us>
2019-04-10 18:01:55 +0200

    stubbed first working version

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from endpoints.embeddings import ns as embeddings_namespace
 
 app = Flask(__name__)
 
-blueprint = Blueprint('api', __name__, url_prefix='/')
+blueprint = Blueprint('api', __name__)
 api.init_app(blueprint)
 api.add_namespace(embeddings_namespace)
-
+app.register_blueprint(blueprint)
 
 if __name__ == '__main__':
     app.run()
--- endpoints/embeddings.py
+++ endpoints/embeddings.py
@@ -1,17 +1,17 @@
+import io
+import numpy as np
 from endpoints import api
 from flask_cors import cross_origin
-from flask import request, abort
-
+from flask import request, abort, send_file
 from machine_learning.extractor import get_seqvec
 from flask_restplus import Resource
 from endpoints.request_models import sequence_post_parameters
 
-
 ns = api.namespace("embeddings", description="Get embeddings")
 
 
-@ns.route('/')
-@cross_origin()
+@ns.route('')
+# @cross_origin()
 class Embeddings(Resource):
     @api.expect(sequence_post_parameters, validate=True)
     @api.response(200, "Calculated embeddings")
@@ -22,11 +22,20 @@ class Embeddings(Resource):
 
         sequence = params.get('sequence')
 
-        if not sequence :
+        if not sequence:
             return abort(400)
-        if len(sequence) > 2000 :
+        if len(sequence) > 2000:
             return abort(400)
 
         embeddings = get_seqvec(sequence)
 
-        return embeddings
+        buffer = io.BytesIO()
+
+        np.savez_compressed(buffer, embeddings)
+
+        buffer.seek(0)  # This simulates closing the file and re-opening it.
+        #  Otherwise the cursor will already be at the end of the
+        #  file when flask tries to read the contents, and it will
+        #  think the file is empty.
+
+        return send_file(buffer, attachment_filename="embedding.npy")
