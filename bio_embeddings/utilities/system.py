29704129a37c47f572a8f5a70facbd6a4bf07a9d
Christian Dallago <code@dallago.us>
2020-02-11 10:09:11 +0100

    added system

--- bio_embeddings/utilities/system.py
+++ bio_embeddings/utilities/system.py
@@ -3,5 +3,14 @@
 import argparse
 from bio_embeddings.utilities.pipeline import run
 
-parser = argparse.ArgumentParser(
-    description='Embeds protein sequences and stores their embeddings as np array.')
+parser = argparse.ArgumentParser(description='Embeds protein sequences.')
+
+parser.add_argument('--overwrite', action='store', dest='overwrite', required=False, default=False, const=True,
+                    help='Will force overwrite of previously existing results.')
+parser.add_argument('config_path', metavar='/path/to/config.yml', type=str, nargs=1, required=True,
+                    help='The path to the config. For examples, see folder "parameter examples".')
+
+arguments = parser.parse_args()
+
+if __name__ == '__main__':
+    run(arguments.config_path)
