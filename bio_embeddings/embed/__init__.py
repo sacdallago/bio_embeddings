e8e18a6f21736cbc2d8ba2044393be86a94394fd
Christian Dallago <code@dallago.us>
2020-04-16 20:11:02 +0200

    stubbed albert stuff

--- bio_embeddings/embed/__init__.py
+++ bio_embeddings/embed/__init__.py
@@ -3,3 +3,5 @@ from bio_embeddings.embed.word2vec import Word2VecEmbedder
 from bio_embeddings.embed.fasttext import FastTextEmbedder
 from bio_embeddings.embed.glove import GloveEmbedder
 from bio_embeddings.embed.transformerxl import TransformerXLEmbedder
+from bio_embeddings.embed.albert import AlbertEmbedder
+
