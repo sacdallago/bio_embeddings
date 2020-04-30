d89538a2020b79470deb16cef529b6d70523031d
Tobias O <tobias.olenyi@tum.de>
2020-04-30 22:07:12 +0200

    Update target python versions

--- setup.py
+++ setup.py
@@ -19,6 +19,9 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
