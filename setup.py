fd31677cb0a94af30362c37aac9d553fa586f499
Tobias O <tobias.olenyi@tum.de>
2020-05-01 18:22:14 +0200

    Update current dependencies

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bio_embeddings",
-    version="0.0.1",
+    version="0.1.0",
     author="Christian Dallago",
     author_email="christian.dallago@tum.de",
     description="A pipeline for protein embedding generation and visualization",
     long_description=long_description,
-    install_requires=["torch", "allennlp", "numpy", "gensim", "biopython", "ruamel_yaml", "pandas", "h5py", "transformers", "plotly", "umap-learn"],
+    install_requires=["torch", "allennlp", "numpy", "gensim", "biopython", "ruamel_yaml", "pandas", "h5py", "transformers", "plotly", "umap-learn", "matplotlib", "scikit-learn", "scipy", "tqdm"],
     scripts=['bio_embeddings/utilities/bio_embeddings'],
     long_description_content_type="text/markdown",
     url="https://github.com/sacdallago/bio_embeddings",
@@ -19,7 +19,6 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9 ",
