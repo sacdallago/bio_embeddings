ecc698a5e09fcd4fd54404030d7c649b58b91f96
Christian Dallago <code@dallago.us>
2020-05-02 16:15:47 +0200

    webserver overhaul

--- webserver/endpoints/request_models.py
+++ webserver/endpoints/request_models.py
@@ -3,7 +3,6 @@ from werkzeug.datastructures import FileStorage
 
 file_post_parser = api.parser()
 file_post_parser.add_argument('sequences', location='files', type=FileStorage, required=True)
-file_post_parser.add_argument('annotations', location='files', type=FileStorage, required=True)
 
 request_status_parser = api.parser()
 request_status_parser.add_argument('id', location='args', help='Job id', required=True)
---
+++ webserver/static/index.js
@@ -0,0 +1,67 @@
+let validateInput = (event) => {
+    console.log(event);
+};
+
+$('select.dropdown').dropdown();
+
+$('#submit-form').form({
+    fields: {
+        sequences: 'empty',
+    },
+    onSuccess(event, fields){
+        event.preventDefault();
+        let errorMessage = $('.job.submission.messages');
+        errorMessage.text("");
+
+        fetch("/api/embeddings", {
+            method: 'post',
+            body: new FormData(event.target),
+        }).then(response => {
+            return response.json();
+        }).then(response => {
+            if(response.message){
+                $('#submit-form').form('add prompt', 'sequences', null);
+                errorMessage.text(response.message);
+            } else {
+                $('#id').val(response.job_id)
+            }
+        }).catch(e => {
+            console.error(e)
+        })
+    }
+});
+
+$('#check-form').form({
+    fields: {
+        id: 'empty',
+    },
+    onSuccess(event, fields) {
+        event.preventDefault();
+
+        let jobIdCard = $('#job-id-status-card');
+        let jobStatus = $('#job-id-status-message');
+        let jobDownloadOptions = $('#job-id-download-options');
+
+        jobIdCard.removeClass('show');
+        jobDownloadOptions.removeClass('show');
+
+        fetch("/api/embeddings/status?id=" + fields.id, {
+            method: 'get'
+        }).then(response => {
+            return response.json();
+        }).then(response => {
+            if(response.message){
+            } else {
+                jobIdCard.addClass('show');
+                jobStatus.text(response.status);
+                if(response.status === "SUCCESS"){
+                    $("#download-embeddings").attr("href", "/api/embeddings?id=" + fields.id);
+                    $("#visualize-embeddings").attr("href", "/visualize/" + fields.id,);
+                    jobDownloadOptions.addClass('show');
+                }
+            }
+        }).catch(e => {
+            console.error(e)
+        })
+    }
+});
