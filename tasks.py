2ccbf1792ff5c5032ed5bca528af7e4b2a4aace9
Tobias O <tobias.olenyi@tum.de>
2020-05-05 18:04:57 +0200

    Remove conda environment

--- tasks.py
+++ tasks.py
@@ -15,12 +15,9 @@ def clean(c, bytecode=False, extra=''):
 @task
 def build(c, update_requirements=True):
     poetry = which("poetry") is not None
-    conda = which("poetry") is not None and os.path.exists(os.path.join(sys.prefix, 'conda-meta'))
     python_command = "poetry run python" if poetry else "python"
 
     if update_requirements:
-        if conda:
-            c.run("conda env export > environment.yml")
         if poetry:
             c.run("poetry export -f requirements.txt > requirements.txt")
         else: 
