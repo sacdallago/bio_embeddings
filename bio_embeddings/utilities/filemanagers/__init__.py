26121168fd8a59810b893c45128358ab944d40e3
Christian Dallago <code@dallago.us>
2020-06-25 17:15:40 +0200

    pacify the lintners

--- bio_embeddings/utilities/filemanagers/__init__.py
+++ bio_embeddings/utilities/filemanagers/__init__.py
@@ -1,15 +1,11 @@
 from bio_embeddings.utilities.filemanagers.FileSystemFileManager import FileSystemFileManager
 from bio_embeddings.utilities.filemanagers.FileManagerInterface import FileManagerInterface
 
-FILE_MANAGERS = {
-    'filesystem': FileSystemFileManager,
-    None: FileSystemFileManager
-}
-
 
 def get_file_manager(**kwargs):
-    management = kwargs.get('management', {})
-    file_manager_type = management.get('file_manager')
-    file_manager = FILE_MANAGERS.get(file_manager_type)
 
-    return file_manager(**management)
+    # A useless call to pacify the lintners
+    # TODO: when new FileManagers are available, parse the file manager type from "management".
+    kwargs.get('management', {})
+
+    return FileSystemFileManager()
