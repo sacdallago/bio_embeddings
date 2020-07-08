632649c3765c80c91d04bb78de4b884cca0d240d
konstin <konstin@mailbox.org>
2020-07-08 16:36:04 +0200

    Remove duplicate code

--- bio_embeddings/embed/pipeline.py
+++ bio_embeddings/embed/pipeline.py
@@ -8,6 +8,7 @@ from Bio import SeqIO
 from pandas import read_csv
 from tqdm import tqdm
 
+from bio_embeddings.embed import EmbedderInterface
 from bio_embeddings.embed.albert import AlbertEmbedder
 from bio_embeddings.embed.seqvec.SeqVecEmbedder import SeqVecEmbedder
 from bio_embeddings.utilities import (
@@ -31,7 +32,7 @@ def _get_reduced_embeddings_file_context(file_manager: FileManagerInterface, res
     """
 
     # Create reduced embeddings file if set in params
-    result_kwargs['reduce'] = result_kwargs.get('reduce', False)
+    result_kwargs.setdefault('reduce', False)
 
     if result_kwargs['reduce'] is True:
         reduced_embeddings_file_path = file_manager.create_file(result_kwargs.get('prefix'),
@@ -43,7 +44,9 @@ def _get_reduced_embeddings_file_context(file_manager: FileManagerInterface, res
     return contextlib.nullcontext()
 
 
-def _get_embeddings_file_context(file_manager: FileManagerInterface, result_kwargs: Dict):
+def _get_embeddings_file_context(
+    file_manager: FileManagerInterface, result_kwargs: Dict[str, Any]
+):
     """
     :param file_manager: The FileManager derived class which will be used to create the file
     :param result_kwargs: A dictionary which will be updated in-place to include the path to the newly created file
@@ -51,19 +54,57 @@ def _get_embeddings_file_context(file_manager: FileManagerInterface, result_kwar
     :return: a file context
     """
 
-    result_kwargs['discard_per_amino_acid_embeddings'] = result_kwargs.get('discard_per_amino_acid_embeddings', False)
+    result_kwargs.setdefault("discard_per_amino_acid_embeddings", False)
 
-    if result_kwargs['discard_per_amino_acid_embeddings'] is True:
-        if result_kwargs['reduce'] is False:
+    if result_kwargs["discard_per_amino_acid_embeddings"] is True:
+        if result_kwargs["reduce"] is False:
             raise InvalidParameterError(
-                "Cannot have discard_per_amino_acid_embeddings=True and reduce=False. Both must be True.")
+                "Cannot have discard_per_amino_acid_embeddings=True and reduce=False. Both must be True."
+            )
+        return contextlib.nullcontext()
     else:
-        embeddings_file_path = file_manager.create_file(result_kwargs.get('prefix'), result_kwargs.get('stage_name'),
-                                                        'embeddings_file', extension='.h5')
-        result_kwargs['embeddings_file'] = embeddings_file_path
+        embeddings_file_path = file_manager.create_file(
+            result_kwargs.get("prefix"),
+            result_kwargs.get("stage_name"),
+            "embeddings_file",
+            extension=".h5",
+        )
+        result_kwargs["embeddings_file"] = embeddings_file_path
         return h5py.File(embeddings_file_path, "w")
 
-    return contextlib.nullcontext()
+
+def embed_and_write_batched(
+    embedder: EmbedderInterface,
+    file_manager: FileManagerInterface,
+    result_kwargs: Dict[str, Any],
+) -> Dict[str, Any]:
+    """ The shared code between the SeqVec and the Albert pipeline """
+    # Lazy fasta file reader. The mapping file contains the corresponding ids in the same order
+    sequences = (
+        str(entry.seq)
+        for entry in SeqIO.parse(result_kwargs["remapped_sequences_file"], "fasta")
+    )
+    mapping_file = read_csv(result_kwargs["mapping_file"], index_col=0)
+    # Open embedding files or null contexts and iteratively save embeddings to file
+    with _get_embeddings_file_context(
+        file_manager, result_kwargs
+    ) as embeddings_file, _get_reduced_embeddings_file_context(
+        file_manager, result_kwargs
+    ) as reduced_embeddings_file:
+        embedding_generator = embedder.embed_many(
+            sequences, result_kwargs["max_amino_acids"]
+        )
+        for sequence_id, embedding in zip(
+            mapping_file.index, tqdm(embedding_generator, total=len(mapping_file))
+        ):
+            if result_kwargs.get("discard_per_amino_acid_embeddings") is False:
+                embeddings_file.create_dataset(sequence_id, data=embedding)
+
+            if result_kwargs.get("reduce") is True:
+                reduced_embeddings_file.create_dataset(
+                    sequence_id, data=embedder.reduce_per_protein(embedding),
+                )
+    return result_kwargs
 
 
 def seqvec(**kwargs) -> Dict[str, Any]:
@@ -72,7 +113,7 @@ def seqvec(**kwargs) -> Dict[str, Any]:
     file_manager = get_file_manager(**kwargs)
 
     # Initialize pipeline and model specific options:
-    result_kwargs["max_amino_acids"] = result_kwargs.get("max_amino_acids", 15000)
+    result_kwargs.setdefault("max_amino_acids", 15000)
 
     if result_kwargs.get("seqvec_version") == 2 or result_kwargs.get("vocabulary_file"):
         necessary_files.append("vocabulary_file")
@@ -94,35 +135,15 @@ def seqvec(**kwargs) -> Dict[str, Any]:
             result_kwargs[file] = file_path
 
     embedder = SeqVecEmbedder(**result_kwargs)
-    # Lazy fasta file reader. The mapping file contains the corresponding ids in the same order
-    sequences = (str(entry.seq) for entry in SeqIO.parse(result_kwargs["remapped_sequences_file"], 'fasta'))
-    mapping_file = read_csv(result_kwargs["mapping_file"], index_col=0)
-
-    # Open embedding files or null contexts and iteratively save embeddings to file
-    with _get_embeddings_file_context(
-        file_manager, result_kwargs
-    ) as embeddings_file, _get_reduced_embeddings_file_context(
-        file_manager, result_kwargs
-    ) as reduced_embeddings_file:
-        embedding_generator = embedder.embed_many(sequences, result_kwargs["max_amino_acids"])
-        for sequence_id, embedding in zip(
-            mapping_file.index, tqdm(embedding_generator, total=len(mapping_file))
-        ):
-            if result_kwargs.get("discard_per_amino_acid_embeddings") is False:
-                embeddings_file.create_dataset(sequence_id, data=embedding)
-
-            if result_kwargs.get("reduce") is True:
-                reduced_embeddings_file.create_dataset(
-                    sequence_id, data=embedder.reduce_per_protein(embedding),
-                )
-
-    return result_kwargs
+    return embed_and_write_batched(embedder, file_manager, result_kwargs)
 
 
 def albert(**kwargs):
     necessary_directories = ["model_directory"]
     result_kwargs = deepcopy(kwargs)
     file_manager = get_file_manager(**kwargs)
+
+    result_kwargs.setdefault("max_amino_acids", 500)
 
     for directory in necessary_directories:
         if not result_kwargs.get(directory):
@@ -137,28 +158,7 @@ def albert(**kwargs):
             result_kwargs[directory] = directory_path
 
     embedder = AlbertEmbedder(**result_kwargs)
-    # Lazy fasta file reader. The mapping file contains the corresponding ids in the same order
-    sequences = (str(entry.seq) for entry in SeqIO.parse(result_kwargs["remapped_sequences_file"], 'fasta'))
-    mapping_file = read_csv(result_kwargs["mapping_file"], index_col=0)
-
-    # Open embedding files or null contexts and iteratively save embeddings to file
-    with _get_embeddings_file_context(
-        file_manager, result_kwargs
-    ) as embeddings_file, _get_reduced_embeddings_file_context(
-        file_manager, result_kwargs
-    ) as reduced_embeddings_file:
-        embedding_generator = embedder.embed_many(sequences, result_kwargs["max_amino_acids"])
-        for sequence_id, embedding in zip(
-            mapping_file.index, tqdm(embedding_generator, total=len(mapping_file))
-        ):
-            if result_kwargs.get("discard_per_amino_acid_embeddings") is False:
-                embeddings_file.create_dataset(sequence_id, data=embedding)
-
-            if result_kwargs.get("reduce") is True:
-                reduced_embeddings_file.create_dataset(
-                    sequence_id, data=embedder.reduce_per_protein(embedding),
-                )
-    return result_kwargs
+    return embed_and_write_batched(embedder, file_manager, result_kwargs)
 
 
 def fasttext(**kwargs):
