19ab34fb85660c9b3fb061ad64fccd38132f7c21
Michael Heinzinger <ga32bav@mytum.de>
2020-07-16 18:59:03 +0200

    Fix batch-embedding in helper.py

--- bio_embeddings/embed/helper.py
+++ bio_embeddings/embed/helper.py
@@ -12,16 +12,24 @@ def embed_batch_berts(
     embedder: EmbedderInterface, batch: List[str]
 ) -> Generator[ndarray, None, None]:
     """ Embed batch code shared between Bert and Albert """
-    batch = [re.sub(r"[UZOB]", "X", sequence) for sequence in batch]
+    batch    = [ re.sub(r"[UZOB]", "X", sequence) for sequence in batch ]
+    seq_lens = [ len(seq)  for seq in batch ]
+    batch    = [ list(seq) for seq in batch ]
+
     ids = embedder._tokenizer.batch_encode_plus(
         batch, add_special_tokens=True, pad_to_max_length=True
     )
-    input_ids = torch.tensor(ids["input_ids"]).to(embedder._device)
+    
+    input_ids      = torch.tensor(ids["input_ids"]).to(embedder._device)
     attention_mask = torch.tensor(ids["attention_mask"]).to(embedder._device)
+    
     with torch.no_grad():
         embedding = embedder._model(input_ids=input_ids, attention_mask=attention_mask)
+        
     embedding = embedding[0].cpu().numpy()
+    
     for seq_num in range(len(embedding)):
-        seq_len = (attention_mask[seq_num] == 1).sum()
-        seq_emd = embedding[seq_num][1: seq_len - 1]
+        seq_len = seq_lens[seq_num]
+        assert seq_len == embedding[seq_num].shape[0]+2 # add two to account for special tokens
+        seq_emd = embedding[seq_num][1:-1] # slice off first and last positions (special tokens)
         yield seq_emd
