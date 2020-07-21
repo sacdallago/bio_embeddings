987501dacede61440bf5239dbaca67645432da09
Christian Dallago <code@dallago.us>
2020-07-21 09:54:40 +0200

    refactor albert embedding dimension according to MH

--- bio_embeddings/embed/albert_embedder.py
+++ bio_embeddings/embed/albert_embedder.py
@@ -16,7 +16,7 @@ from bio_embeddings.utilities import (
 
 class AlbertEmbedder(EmbedderInterface):
     name = "albert"
-    embedding_dimension = 1024
+    embedding_dimension = 4096
     number_of_layers = 1
 
     def __init__(self, **kwargs):
