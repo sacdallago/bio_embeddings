0c288ec53b164c5926b3695da322a21551e4b020
Christian Dallago <code@dallago.us>
2020-08-26 14:39:55 +0200

    implementing unannotated extract

--- bio_embeddings/extract/pipeline.py
+++ bio_embeddings/extract/pipeline.py
@@ -1,5 +1,6 @@
 import logging
 import h5py
+import torch
 import numpy as np
 from copy import deepcopy
 
@@ -16,13 +17,72 @@ from bio_embeddings.utilities.exceptions import InvalidParameterError
 logger = logging.getLogger(__name__)
 
 
-def unsupervised(**kwargs) -> Dict[str, Any]:
-    raise NotImplementedError()
+# From https://github.com/josipd/torch-two-sample/blob/master/torch_two_sample/util.py
+# and https://github.com/Rostlab/goPredSim/blob/master/two_sample_util.py
+def _pairwise_distance(sample_1: torch.Tensor, sample_2: torch.Tensor, norm: int = 2, eps=1e-5):
+    r"""Compute the matrix of all squared pairwise distances.
+    Arguments
+    ---------
+    sample_1 : torch.Tensor or Variable
+        The first sample, should be of shape ``(n_1, d)``.
+    sample_2 : torch.Tensor or Variable
+        The second sample, should be of shape ``(n_2, d)``.
+    norm : float
+        The l_p norm to be used.
+    eps :
+    Returns
+    -------
+    torch.Tensor or Variable
+        Matrix of shape (n_1, n_2). The [i, j]-th entry is equal to
+        ``|| sample_1[i, :] - sample_2[j, :] ||_p``."""
+    n_1, n_2 = sample_1.size(0), sample_2.size(0)
+    norm = float(norm)
+    if norm == 2.:
+        norms_1 = torch.sum(sample_1**2, dim=1, keepdim=True)
+        norms_2 = torch.sum(sample_2**2, dim=1, keepdim=True)
+        norms = (norms_1.expand(n_1, n_2) +
+                 norms_2.transpose(0, 1).expand(n_1, n_2))
+        distances_squared = norms - 2 * sample_1.mm(sample_2.t())
+        return torch.sqrt(eps + torch.abs(distances_squared))
+    else:
+        dim = sample_1.size(1)
+        expanded_1 = sample_1.unsqueeze(1).expand(n_1, n_2, dim)
+        expanded_2 = sample_2.unsqueeze(0).expand(n_1, n_2, dim)
+        differences = torch.abs(expanded_1 - expanded_2) ** norm
+        inner = torch.sum(differences, dim=2, keepdim=False)
+        return (eps + inner) ** (1. / norm)
 
-    # TODO: pick up from here
-    check_required(kwargs, ['reference_embeddings', 'reference_annotations'])
+
+def unsupervised(**kwargs) -> Dict[str, Any]:
+    check_required(kwargs, ['reference_embeddings_file', 'reference_annotations_file', 'reduced_embeddings_file'])
 
     result_kwargs = deepcopy(kwargs)
+    file_manager = get_file_manager(**kwargs)
+
+    # Try to create final files (if this fails, now is better than later
+    transferred_annotations_file = file_manager.create_file(result_kwargs.get('prefix'),
+                                                            result_kwargs.get('stage_name'),
+                                                            'transferred_annotations_file',
+                                                            extension='.csv')
+
+    # Read the reference annotations and reference embeddings
+
+    # The reference annotations file must be CSV containing two columns & headers like:
+    # identifier,label
+    # ** identifier doesn't need to be unique **
+    reference_annotations_file = read_csv(result_kwargs['reference_annotations_file'])
+
+    # Starting from here order is super important!
+    reference_identifiers = reference_annotations_file['identifier'].unique().sort()
+    reference_embeddings = list()
+
+    # Only read in embeddings for annotated sequences! This will save RAM/GPU_RAM.
+    with h5py.File(result_kwargs['reference_embeddings_file'], 'r') as reference_embeddings_file:
+        for identifier in reference_identifiers:
+            reference_embeddings.append(reference_embeddings_file[identifier])
+
+    # mapping file will be needed to transfer annotations
+    mapping_file = read_csv(result_kwargs['mapping_file'], index_col=0)
 
     return result_kwargs
 
