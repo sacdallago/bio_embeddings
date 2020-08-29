3fd1675b86b207269f8d1a7f4107259072eb4db0
Christian Dallago <code@dallago.us>
2020-08-29 15:02:58 +0200

    Interpret index as string

--- bio_embeddings/extract/pipeline.py
+++ bio_embeddings/extract/pipeline.py
@@ -128,7 +128,7 @@ def unsupervised(**kwargs) -> Dict[str, Any]:
     mapping_file = read_csv(result_kwargs['mapping_file'], index_col=0)
 
     # Important to have consistent ordering!
-    target_identifiers = mapping_file.index.values
+    target_identifiers = mapping_file.index.map(str).values
     target_identifiers.sort()
     target_embeddings = list()
 
