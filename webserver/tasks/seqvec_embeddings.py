917b0761c1973775e8205ae114014a26957c7425
Christian Dallago <code@dallago.us>
2020-10-26 12:55:10 +0100

    serialize answer

--- webserver/tasks/seqvec_embeddings.py
+++ webserver/tasks/seqvec_embeddings.py
@@ -11,6 +11,7 @@ featureExtractor = None
 if configuration['celery']['celery_worker_type'] == "seqvec":
     from bio_embeddings.embed import SeqVecEmbedder
     from bio_embeddings.extract.basic.BasicAnnotationExtractor import BasicAnnotationExtractor
+    from bio_embeddings.utilities import convert_list_of_enum_to_string
 
     logger.info("Loading the language model...")
 
@@ -40,4 +41,10 @@ def get_seqvec_annotations_sync(sequence: str):
     embedding = model.embed(sequence)
     annotations = featureExtractor.get_annotations(embedding)
 
-    return annotations
+    return {
+        "predictedDSSP3": convert_list_of_enum_to_string(annotations.DSSP3),
+        "predictedDSSP8": convert_list_of_enum_to_string(annotations.DSSP8),
+        "predictedDisorder": convert_list_of_enum_to_string(annotations.disorder),
+        "predictedMembrane": annotations.membrane.value,
+        "predictedSubcellularLocalizations": annotations.localization.value,
+    }
