a89e333ae244d4eca8abd792bf7bb9ef898f3ad3
Christian Dallago <code@dallago.us>
2020-11-03 17:25:54 +0100

    export 2D scatter init

--- bio_embeddings/visualize/__init__.py
+++ bio_embeddings/visualize/__init__.py
@@ -1,1 +1,3 @@
-from bio_embeddings.visualize.plotly_plots import render_3D_scatter_plotly, save_plotly_figure_to_html, render_scatter_plotly
+from bio_embeddings.visualize.plotly_plots import (
+    render_3D_scatter_plotly, save_plotly_figure_to_html, render_scatter_plotly
+)
