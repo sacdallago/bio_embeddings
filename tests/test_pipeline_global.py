79690de92044cc892601be8fbc72ee12e364a125
Konstantin Schütze <schuetze@in.tum.de>
2020-11-29 13:39:20 +0100

    Fix tests without install

--- tests/test_pipeline_global.py
+++ tests/test_pipeline_global.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import importlib_metadata
 import toml
 
 from bio_embeddings.utilities.pipeline import execute_pipeline_from_config
@@ -16,18 +17,23 @@ def test_pipeline_global(tmp_path):
         }
     )
 
-    actual = prefix.joinpath("bio_embeddings_version.txt").read_text()
-    expected = toml.loads(Path("pyproject.toml").read_text())["tool"]["poetry"][
-        "version"
-    ]
-    assert actual == expected
-
     expected_files = [
-        "bio_embeddings_version.txt",
         "input_parameters_file.yml",
         "mapping_file.csv",
         "ouput_parameters_file.yml",
         "remapped_sequences_file.fasta",
         "sequences_file.fasta",
     ]
-    assert expected_files == sorted(path.name for path in prefix.iterdir())
+
+    try:
+        importlib_metadata.version("bio_embeddings")
+        actual = prefix.joinpath("bio_embeddings_version.txt").read_text()
+        expected = toml.loads(Path("pyproject.toml").read_text())["tool"]["poetry"][
+            "version"
+        ]
+        assert actual == expected
+        expected_files.append("bio_embeddings_version.txt")
+    except importlib_metadata.PackageNotFoundError:
+        pass  # No dev install
+
+    assert sorted(expected_files) == sorted(path.name for path in prefix.iterdir())
