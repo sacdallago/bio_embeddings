7f78d0fbbc79d28cab249797b8985106681f4eb0
Christian Dallago <code@dallago.us>
2020-12-03 17:21:07 +0100

    correct queue name

--- webserver/tasks/__init__.py
+++ webserver/tasks/__init__.py
@@ -9,6 +9,6 @@ task_keeper.conf.task_routes = {
     'webserver.tasks.seqvec_embeddings.get_seqvec_embeddings_sync': {'queue': 'seqvec'},
     'webserver.tasks.seqvec_annotations.get_seqvec_annotations_sync': {'queue': 'seqvec_annotations'},
     'webserver.tasks.protbert_embeddings.get_protbert_embeddings_sync': {'queue': 'protbert'},
-    'webserver.tasks.protbert_annotations.get_seqvec_annotations_sync': {'queue': 'protbert_annotations'},
+    'webserver.tasks.protbert_annotations.get_protbert_annotations_sync': {'queue': 'protbert_annotations'},
     'webserver.tasks.pipeline.run_pipeline': {'queue': 'pipeline'},
 }
