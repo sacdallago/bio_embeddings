a5fd1bc729867d8f5938a733a4baad4067ea02d6
Christian Dallago <code@dallago.us>
2020-12-17 18:39:04 +0100

    consider 7 neighbours by default

--- webserver/tasks/seqvec_annotations.py
+++ webserver/tasks/seqvec_annotations.py
@@ -15,7 +15,7 @@ BPO_annotations = None
 CCO_annotations = None
 MFO_annotations = None
 metric = "euclidean"
-k = 3
+k = 7
 
 if "seqvec_annotations" in configuration['celery']['celery_worker_type']:
     from bio_embeddings.extract.basic.BasicAnnotationExtractor import BasicAnnotationExtractor
