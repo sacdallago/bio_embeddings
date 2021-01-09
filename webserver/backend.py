5886dd9ee9495524dc9209f2567bd60eedb5996a
Konstantin Schütze <schuetze@in.tum.de>
2021-01-09 14:02:59 +0100

    Add basic test for the webserver

--- webserver/backend.py
+++ webserver/backend.py
@@ -3,47 +3,60 @@ from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from webserver.endpoints import api
-from webserver.endpoints.pipeline import ns as pipeline_namespace
-from webserver.endpoints.embeddings import ns as embeddings_namespace
 from webserver.endpoints.annotations import ns as annotations_namespace
-
+from webserver.endpoints.embeddings import ns as embeddings_namespace
+from webserver.endpoints.pipeline import ns as pipeline_namespace
+from webserver.tasks import task_keeper
 from webserver.utilities.configuration import configuration
 
-from webserver.tasks import task_keeper
 
-# Initialize API
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# Required parameters
-app.config['MAX_CONTENT_LENGTH'] = configuration['web']['max_content_length']
+    # Required parameters
+    app.config["MAX_CONTENT_LENGTH"] = configuration["web"]["max_content_length"]
 
-blueprint = Blueprint('api', __name__, url_prefix='/api')
-cors = CORS(blueprint, origins=['https://embed.protein.properties', 'http://localhost:3000',
-                                'https://predictprotein.org', 'https://predictprotein.test',
-                                'https://bioembeddings.com', 'https://seqvec.bioembeddings.com',
-                                'https://login.predictprotein.org'
-                                ])
+    blueprint = Blueprint("api", __name__, url_prefix="/api")
+    CORS(
+        blueprint,
+        origins=[
+            "https://embed.protein.properties",
+            "http://localhost:3000",
+            "https://predictprotein.org",
+            "https://predictprotein.test",
+            "https://bioembeddings.com",
+            "https://seqvec.bioembeddings.com",
+            "https://login.predictprotein.org",
+        ],
+    )
 
-api.init_app(blueprint)
-api.add_namespace(pipeline_namespace)
-api.add_namespace(embeddings_namespace)
-api.add_namespace(annotations_namespace)
+    api.init_app(blueprint)
+    api.add_namespace(pipeline_namespace)
+    api.add_namespace(embeddings_namespace)
+    api.add_namespace(annotations_namespace)
 
-app.register_blueprint(blueprint)
+    app.register_blueprint(blueprint)
 
-# https://flask.palletsprojects.com/en/1.1.x/quickstart/#hooking-in-wsgi-middleware
-app.wsgi_app = ProxyFix(app.wsgi_app)
+    # https://flask.palletsprojects.com/en/1.1.x/quickstart/#hooking-in-wsgi-middleware
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+
+    @app.route("/")
+    def index():
+        i = task_keeper.control.inspect()
+        queues = set()
+        active_queues = i.active_queues() or dict()
+        for queue in active_queues:
+            queues.add(active_queues[queue][0]["name"])
+        return render_template("index.html", workers=queues)
+
+    return app
 
 
-@app.route("/")
-def index():
-    i = task_keeper.control.inspect()
-    queues = set()
-    active_queues = i.active_queues() or dict()
-    for queue in active_queues:
-        queues.add(active_queues[queue][0]['name'])
-    return render_template("index.html", workers=queues)
+def main():
+    app = create_app()
+    # This is for dev only, gunicorn will run without debug
+    app.run(debug=True, host="0.0.0.0")
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    main()
