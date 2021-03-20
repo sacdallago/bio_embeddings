8b0ef7847fc7b410cfc53bd798c53da42978032c
Hannes Stärk <hannes.staerk@tum.de>
2021-03-20 07:28:09 +0000

    Remove merge artefact

--- bio_embeddings/extract/annotations/__init__.py
+++ bio_embeddings/extract/annotations/__init__.py
@@ -2,7 +2,6 @@ from bio_embeddings.extract.annotations.Disorder import Disorder
 from bio_embeddings.extract.annotations.Location import Location
 from bio_embeddings.extract.annotations.Membrane import Membrane
 from bio_embeddings.extract.annotations.SecondaryStructure import SecondaryStructure
-from bio_embeddings.extract.basic.BasicAnnotationExtractor import SubcellularLocalizationAndMembraneBoundness
 
 __all__ = ["Disorder", "Location", "Membrane", "SecondaryStructure"]
 
