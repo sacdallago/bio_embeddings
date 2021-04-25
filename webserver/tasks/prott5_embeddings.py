e482e32dd9ee550b53753c44a0324e44609ac6da
Christian Dallago <code@dallago.us>
2021-04-25 13:43:16 +0200

    change embedder

--- webserver/tasks/prott5_embeddings.py
+++ webserver/tasks/prott5_embeddings.py
@@ -10,14 +10,14 @@ logger = logging.getLogger(__name__)
 model = None
 
 if "prott5" in configuration['celery']['celery_worker_type']:
-    from bio_embeddings.embed import ProtTransT5BFDEmbedder
+    from bio_embeddings.embed import ProtTransT5XLU50
 
     logger.info("Loading the language model...")
 
-    model = ProtTransT5BFDEmbedder(
+    model = ProtTransT5XLU50(
         model_directory=configuration['prottrans_t5_xl_u50']['model_directory'],
         decoder=False,
-        half_precision_model=False
+        half_precision_model=True
     )
 
     logger.info("Finished initializing.")
