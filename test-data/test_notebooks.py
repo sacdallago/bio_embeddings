20fe92957bf9277975199463622f8d2fae3894af
Konstantin Schütze <schuetze@in.tum.de>
2021-08-02 15:40:20 +0200

    Add a script that tests the notebooks

---
+++ test-data/test_notebooks.py
@@ -0,0 +1,43 @@
+"""
+Runs all notebooks, skipping the pip install part. Run as
+
+```
+python -m test-data.test_notebooks
+```
+"""
+
+import json
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from tqdm import tqdm
+
+
+def remove_pip_install_from_notebook(notebook):
+    # Remove pip install calls because we want to test the local version
+    cells = []
+    for cell in notebook["cells"]:
+        if "pip3 install" in "".join(cell["source"]) or "pip install" in "".join(
+            cell["source"]
+        ):
+            continue
+        cells.append(cell)
+    notebook["cells"] = cells
+
+
+def main():
+    notebooks = list(Path("notebooks").glob("*.ipynb"))
+    for file in tqdm(notebooks):
+        print(f"Running {file}")
+        notebook = json.loads(file.read_text())
+        remove_pip_install_from_notebook(notebook)
+        nb = nbformat.reads(json.dumps(notebook), as_version=4)
+        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+        with TemporaryDirectory() as temp_dir:
+            ep.preprocess(nb, {"metadata": {"path": temp_dir}})
+
+
+if __name__ == "__main__":
+    main()
