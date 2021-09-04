30c578098e2cb978105c2477a0e7b1b7728075f9
Christian Dallago <chdallago@Christians-MBP.fritz.box>
2021-09-04 16:53:11 +0200

    update init

--- bio_embeddings/align/__init__.py
+++ bio_embeddings/align/__init__.py
@@ -1,4 +1,12 @@
 from .deepblast import deepblast_align
+from .mmseqs2 import (
+    check_mmseqs, convert_mmseqs_result_to_profile,
+    create_mmseqs_database, mmseqs_search, MMseqsSearchOptions,
+    MMseqsSearchOptionsEnum
+)
 from .pipeline import run
 
-__all__ = ["deepblast_align", "run"]
+__all__ = [
+    "deepblast_align", "check_mmseqs", "convert_mmseqs_result_to_profile",
+    "create_mmseqs_database", "mmseqs_search", "MMseqsSearchOptions", "MMseqsSearchOptionsEnum", "run"
+]
