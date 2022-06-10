012a4ab5157444b9a89ed460f4498f14182932ed
Benjamin Bjoern Kroeger <kroegerbe@cip.ifi.lmu.de>
2022-06-10 09:19:31 +0200

    moved retreival of embeddings to the task interface

--- webserver/endpoints/vespa.py
+++ webserver/endpoints/vespa.py
@@ -7,7 +7,6 @@ from flask_restx import Resource
 from webserver.endpoints import api
 from webserver.endpoints.request_models import vespa_post_parameters
 from webserver.endpoints.task_interface import get_residue_landscape
-from webserver.endpoints.task_interface import get_embedding
 from webserver.endpoints.utils import check_valid_sequence
 
 ns = api.namespace("VESPA", description="Calculate SAV effect")
@@ -28,14 +27,9 @@ class vespa(Resource):
         if not sequence or len(sequence) > 2000 or not check_valid_sequence(sequence):
             return abort(400, "Sequence is too long or contains invalid characters.")
 
-        model_name = 'prottrans_t5_xl_u50'
-
-        embedding = get_embedding(model_name, sequence)
-        embedding = embedding.tolist()
-
-        residue_landscape_output = get_residue_landscape(sequence=sequence,model_name='prottrans_t5_xl_u50', embedding_as_list=embedding)
+        residue_landscape_output = get_residue_landscape(model_name='prottrans_t5_xl_u50',sequence=sequence)
         cons_pred = residue_landscape_output['predictedConservation']
         vespa_out = residue_landscape_output['predictedVariation']
 
-        return {'predictedVariation':vespa_out,
-                'predictedConservation':cons_pred}
+        return {'predictedVariation': vespa_out,
+                'predictedConservation': cons_pred}
