504e03f0421a4730a062fe66cacbb106580b0a44
Benjamin Bjoern Kroeger <kroegerbe@cip.ifi.lmu.de>
2022-06-16 12:25:32 +0200

    Added extra check if the residue landscape worker shall be loaded

--- webserver/celery_worker.py
+++ webserver/celery_worker.py
@@ -8,8 +8,9 @@ if "prott5" in configuration['celery']['celery_worker_type']:
 if "prott5_annotations" in configuration['celery']['celery_worker_type']:
     # noinspection PyUnresolvedReferences
     from webserver.tasks.prott5_annotations import get_prott5_annotations_sync
+if "prott5_residue_landscape_annotations" in configuration['celery']['celery_worker_type']:
     # add ann extra worker to compute the residue landscape
-    from webserver.tasks.prott5_SAV_effect import get_residue_landscape_output_sync
+    from webserver.tasks.prott5_residue_landscape_annotations import get_residue_landscape_output_sync
 if "pipeline" in configuration['celery']['celery_worker_type']:
     # noinspection PyUnresolvedReferences
     from webserver.tasks.pipeline import run_pipeline
