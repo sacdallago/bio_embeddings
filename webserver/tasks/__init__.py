dcc5d76fe7ebd4758b30188a185fba94721b88be
Benjamin Bjoern Kroeger <kroegerbe@cip.ifi.lmu.de>
2022-06-16 12:22:39 +0200

    Changed queue name and File name

--- webserver/tasks/__init__.py
+++ webserver/tasks/__init__.py
@@ -9,5 +9,5 @@ task_keeper.conf.task_routes = {
     'webserver.tasks.prott5_embeddings.get_prott5_embeddings_sync': {'queue': 'prott5'},
     'webserver.tasks.prott5_annotations.get_prott5_annotations_sync': {'queue': 'prott5_annotations'},
     'webserver.tasks.pipeline.run_pipeline': {'queue': 'pipeline'},
-    'webserver.tasks.prott5_SAV_effect.get_residue_landscape_output_sync':{'queue':'SAV'}
+    'webserver.tasks.prott5_residue_landscape_annotations.get_residue_landscape_output_sync':{'queue':'prott5_residue_landscape_annotations'}
 }
